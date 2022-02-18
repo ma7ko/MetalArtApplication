f6cd843f8092563d52fa5e828f2788db4ff0ee53
Marko Panushkovski <marko.panushkovski@pelagus-it.com>
2022-02-18 22:42:56 +0100

    initial commit on server application

--- src/main/java/mk/ukim/finki/metalartapplication/service/MailService.java
+++ src/main/java/mk/ukim/finki/metalartapplication/service/MailService.java
@@ -2,9 +2,12 @@ package mk.ukim.finki.metalartapplication.service;
 
 import mk.ukim.finki.metalartapplication.model.Mail;
 import mk.ukim.finki.metalartapplication.model.dto.RegistrationRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 
 public interface MailService {
     public void sendVerifyEmail(RegistrationRequest request) throws MessagingException;
+    public void sendSketchEmail(MultipartFile uploadedFile, String name, String description, Double width, Double height, Double depth, String metric) throws MessagingException, IOException;
 }
--- src/main/java/mk/ukim/finki/metalartapplication/service/impl/MailServiceImpl.java
+++ src/main/java/mk/ukim/finki/metalartapplication/service/impl/MailServiceImpl.java
@@ -3,14 +3,29 @@ package mk.ukim.finki.metalartapplication.service.impl;
 import mk.ukim.finki.metalartapplication.model.Mail;
 import mk.ukim.finki.metalartapplication.model.dto.RegistrationRequest;
 import mk.ukim.finki.metalartapplication.service.MailService;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.StringUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.apache.commons.io.*;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,6 +59,31 @@ public class MailServiceImpl implements MailService {
         mimeMessageHelper.setText(html, true);
         this.mailSender.send(mimeMessageHelper.getMimeMessage());
 
+    }
+
+    @Override
+    public void sendSketchEmail(MultipartFile uploadedFile, String name, String description, Double width, Double height, Double depth, String metric) throws MessagingException, IOException {
+        MimeMessage mimeMessage = this.mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        FileUtils.writeByteArrayToFile(new File("D:\\Desktop\\file.svg"), uploadedFile.getBytes());
+        mimeMessageHelper.setSubject("Product Request");
+        mimeMessageHelper.setFrom("markopanuskovski@yahoo.com");
+        mimeMessageHelper.setTo("markopanuskovski@gmail.com");
+        FileSystemResource fsr = new FileSystemResource(new File("D:\\Desktop\\file.svg"));
+        mimeMessageHelper.addAttachment(fsr.getFilename(), fsr);
+
+        Context context = new Context();
+        context.setVariable("title", name);
+        context.setVariable("description", description);
+        context.setVariable("width", width);
+        context.setVariable("height", height);
+        context.setVariable("depth", depth);
+        context.setVariable("metric", metric);
+
+        String html = this.templateEngine.process("product-request", context);
+        mimeMessageHelper.setText(html, true);
+        this.mailSender.send(mimeMessageHelper.getMimeMessage());
     }
 
 }
